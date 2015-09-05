551744f33e82ad73f7e51f9b589291dd45be7ffc
PeeHaa <info@pieterhordijk.com>
2015-09-05 18:06:54 +0200

    Added cached routes

---
+++ cache/routes.php
@@ -0,0 +1,26 @@
+<?php return array (
+  0 => 
+  array (
+    'GET' => 
+    array (
+      '/not-found' => 
+      array (
+        0 => 'Demo\\Presentation\\Controller\\Error',
+        1 => 'notFound',
+      ),
+      '/method-not-allowed' => 
+      array (
+        0 => 'Demo\\Presentation\\Controller\\Error',
+        1 => 'methodNotAllowed',
+      ),
+      '/' => 
+      array (
+        0 => 'Demo\\Presentation\\Controller\\Index',
+        1 => 'index',
+      ),
+    ),
+  ),
+  1 => 
+  array (
+  ),
+);
