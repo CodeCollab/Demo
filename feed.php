ef66bce3d93fd6151746fadf8b65db0befc8a1f9
PeeHaa <info@pieterhordijk.com>
2015-09-05 18:42:26 +0200

    Manually feeding super globals

---
+++ feed.php
@@ -0,0 +1,45 @@
+<?php
+
+$_SERVER = array (
+  'HTTP_HOST' => 'codecollabdemo.localhost',
+  'HTTP_CONNECTION' => 'keep-alive',
+  'HTTP_PRAGMA' => 'no-cache',
+  'HTTP_CACHE_CONTROL' => 'no-cache',
+  'HTTP_USER_AGENT' => 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2500.0 Safari/537.36',
+  'HTTP_ACCEPT' => '*/*',
+  'HTTP_REFERER' => 'http://codecollabdemo.localhost/',
+  'HTTP_ACCEPT_ENCODING' => 'gzip, deflate, sdch',
+  'HTTP_ACCEPT_LANGUAGE' => 'nl-NL,nl;q=0.8,en-US;q=0.6,en;q=0.4',
+  'PATH' => 'C:\\ProgramData\\Oracle\\Java\\javapath;C:\\Program Files\\ImageMagick-6.8.7-Q16;C:\\Program Files\\Common Files\\Microsoft Shared\\Windows Live;C:\\Program Files (x86)\\Common Files\\Microsoft Shared\\Windows Live;C:\\Program Files\\PHP\\7.0.0rc2\\;C:\\Program Files\\PHP\\7.0.0rc2\\ext;C:\\windows\\system32;C:\\windows;C:\\windows\\System32\\Wbem;C:\\windows\\System32\\WindowsPowerShell\\v1.0\\;C:\\Program Files (x86)\\Common Files\\Roxio Shared\\DLLShared\\;C:\\Program Files (x86)\\Windows Live\\Shared;C:\\Program Files (x86)\\Android SDK\\tools;C:\\Users\\Gerrie\\Desktop\\ph\\Websites\\bin;C:\\Program Files\\TortoiseHg\\;C:\\Program Files (x86)\\Microsoft Visual Studio 9.0\\VC\\bin\\;C:\\Program Files (x86)\\Microsoft Visual Studio 9.0\\Common7\\IDE;C:\\Program Files\\Microsoft SDKs\\Windows\\v6.0A\\Lib\\x64;C:\\ProgramData\\Composer\\bin;C:\\Program Files\\TortoiseSVN\\bin;C:\\HashiCorp\\Vagrant\\bin;C:\\Program Files (x86)\\Git\\cmd;',
+  'SystemRoot' => 'C:\\windows',
+  'COMSPEC' => 'C:\\windows\\system32\\cmd.exe',
+  'PATHEXT' => '.COM;.EXE;.BAT;.CMD;.VBS;.VBE;.JS;.JSE;.WSF;.WSH;.MSC',
+  'WINDIR' => 'C:\\windows',
+  'SERVER_SIGNATURE' => '',
+  'SERVER_SOFTWARE' => 'Apache/2.4.12 (Win64) PHP/7.0.0RC2',
+  'SERVER_NAME' => 'codecollabdemo.localhost',
+  'SERVER_ADDR' => '::1',
+  'SERVER_PORT' => '80',
+  'REMOTE_ADDR' => '::1',
+  'DOCUMENT_ROOT' => 'C:/Users/Gerrie/Desktop/ph/Websites/CodeCollabLib/Demo/public',
+  'REQUEST_SCHEME' => 'http',
+  'CONTEXT_PREFIX' => '',
+  'CONTEXT_DOCUMENT_ROOT' => 'C:/Users/Gerrie/Desktop/ph/Websites/CodeCollabLib/Demo/public',
+  'SERVER_ADMIN' => 'info@pieterhordijk.com',
+  'SCRIPT_FILENAME' => 'C:/Users/Gerrie/Desktop/ph/Websites/CodeCollabLib/Demo/public/index.php',
+  'REMOTE_PORT' => '1424',
+  'GATEWAY_INTERFACE' => 'CGI/1.1',
+  'SERVER_PROTOCOL' => 'HTTP/1.1',
+  'REQUEST_METHOD' => 'GET',
+  'QUERY_STRING' => '',
+  'REQUEST_URI' => '/favicon.ico',
+  'SCRIPT_NAME' => '/index.php',
+  'PHP_SELF' => '/index.php',
+  'REQUEST_TIME_FLOAT' => 1441377960.8410001,
+  'REQUEST_TIME' => 1441377960,
+);
+
+$_GET = [];
+$_POST = [];
+$_FILES = [];
+$_COOKIE = [];
