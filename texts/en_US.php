6a01d68759ad57ac3be3d911fcd2784e9db7c367
PeeHaa <info@pieterhordijk.com>
2015-09-13 20:55:07 +0200

    Added texts

--- texts/en_US.php
+++ texts/en_US.php
@@ -2,4 +2,29 @@
 
 $texts = [
     'site.title'                                            => 'CodeCollab Demo Application',
+
+    'request.error.not-found.title'                         => '404',
+    'request.error.not-found.description'                   => 'The page does not exist!',
+    'request.error.generic.title'                           => 'Error',
+    'request.error.generic.description'                     => 'Something went wrong :-(',
+
+    'form.error.required'                                   => 'This field is required',
+
+    'header.menu.toggle'                                    => 'Toggle navigation',
+    'header.menu.current'                                   => 'current',
+    'header.menu.item.home'                                 => 'Home',
+    'header.menu.item.logout.%'                             => 'Log out %s',
+    'footer.copyright.%'                                    => 'Copyright © %s CodeCollab',
+
+    'form.login.label.username'                             => 'Username',
+    'form.login.placeholder.username'                       => 'demo',
+    'form.login.label.password'                             => 'Password',
+    'form.login.placeholder.password'                       => 'demo',
+    'form.login.label.rememberme'                           => 'Remember me',
+    'form.login.label.submit'                               => 'Log in',
+    'form.login.error.title'                                => 'Invalid credentials!',
+    'form.login.error.description'                          => 'The supplied credentials were not valid.',
+
+    'page.home.title'                                       => 'Demo',
+    'page.home.description'                                 => 'This is the demo',
 ];
